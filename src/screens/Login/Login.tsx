02deef87314afa26f11d1f59dd0edaa0530bdebe
Umair Ahmed <uahmed@and.digital>
2024-12-10 20:58:06 +0000

    clean up login screen

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -2,37 +2,18 @@ import { Button, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
 import { useTheme } from '@/theme';
-import { Paths } from '@/navigation/paths';
 
 import { SafeScreen } from '@/components/templates';
 
 import { login } from '@/api';
 import { saveUser } from '@/redux/actions/userActions';
 
-function Login({ navigation, user, saveUser }: any) {
+function Login({ user, saveUser }: any) {
   const { fonts, layout } = useTheme();
-  const isUserLoggedIn = user?.isLoggedIn;
-  console.log({ isUserLoggedIn });
-  const navigatePostAppLogin = async () => {
-    navigation.reset({
-      index: 0,
-      routes: [{ name: Paths.Home }],
-    });
-  };
 
   const doLogin = async () => {
-    try {
-      if (isUserLoggedIn) {
-        // Suppose we'll run the biometrics here...
-        navigatePostAppLogin();
-        return;
-      }
-      const userData = await login();
-      saveUser({ ...userData, isLoggedIn: true });
-      navigatePostAppLogin();
-    } catch (error: any) {
-      console.error(error?.message);
-    }
+    const userData = await login();
+    saveUser({ ...userData, isLoggedIn: true });
   };
 
   return (
@@ -46,10 +27,7 @@ function Login({ navigation, user, saveUser }: any) {
         ]}
       >
         <Text style={[fonts.size_16, fonts.gray800]}>Hello, {user?.name}</Text>
-        <Button
-          title={isUserLoggedIn ? 'Access your app' : 'Login'}
-          onPress={doLogin}
-        />
+        <Button title="Login" onPress={doLogin} />
       </View>
     </SafeScreen>
   );
