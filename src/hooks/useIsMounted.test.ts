a74b07c45380993da91232dfb88341ff408be4d7
Umair Ahmed <uahmed@and.digital>
2024-12-11 01:40:30 +0000

    add test for hook

---
+++ src/hooks/useIsMounted.test.ts
@@ -0,0 +1,22 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useIsMounted from './useIsMounted';
+
+describe('useIsMounted', () => {
+  it('should return true when the component is mounted', () => {
+    const { result } = renderHook(() => useIsMounted());
+
+    // When the hook is first used, it should return true
+    expect(result.current()).toBe(true);
+  });
+
+  it('should return false when the component is unmounted', () => {
+    const { result, unmount } = renderHook(() => useIsMounted());
+
+    expect(result.current()).toBe(true);
+
+    unmount();
+
+    expect(result.current()).toBe(false);
+  });
+});
