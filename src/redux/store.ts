e2ebc7ca33d27454bb97ab85edb76d01f7f493f5
Umair Ahmed <uahmed@and.digital>
2024-12-12 22:13:15 +0000

    migrate redux persist to MMKV from async storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,30 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MMKV } from 'react-native-mmkv';
 import { applyMiddleware, createStore } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
+import { persistReducer, persistStore, Storage } from 'redux-persist';
 import promise from 'redux-promise-middleware';
 
 import rootReducer from './reducers';
 
+const storage = new MMKV();
+
+export const reduxStorage: Storage = {
+  setItem: (key, value) => {
+    storage.set(key, value);
+    return Promise.resolve(true);
+  },
+  getItem: (key) => {
+    const value = storage.getString(key);
+    return Promise.resolve(value);
+  },
+  removeItem: (key) => {
+    storage.delete(key);
+    return Promise.resolve();
+  },
+};
+
 const persistConfig = {
   key: 'root',
-  storage: AsyncStorage,
+  storage: reduxStorage,
   whitelist: ['user'],
 };
 
