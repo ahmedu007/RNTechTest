34ce7b4d794f1bb55a72ad1ec40c6b4d1b80392f
Umair Ahmed <uahmed@and.digital>
2024-12-12 22:13:00 +0000

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
+  preset: 'react-native',
   collectCoverageFrom: [
     '<rootDir>/src/Components/**/*.{jsx, tsx}',
     '<rootDir>/src/App.{jsx, tsx}',
   ],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  preset: 'react-native',
+
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/*.test.ts?(x)', '**/*.test.js?(x)'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation|ky)',
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation|ky|react-redux)',
   ],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 import './__mocks__/libs';
 import './__mocks__/getAssetsContext';
+
+jest.unstable_mockModule('react-redux', () => {
+  return {
+    ...jest.requireActual('react-redux'),
+    useSelector: jest.fn().mockImplementation(() => ({})),
+    useDispatch: () => jest.fn(),
+  };
+});
